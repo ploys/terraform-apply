f85da8f2a457a75dcf162cbd5e24554db9b5ef88
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-09 17:21:30 +0100

    Add support for plan file decryption (#3)

--- tests/index.ts
+++ tests/index.ts
@@ -10,6 +10,16 @@ import { run } from '../src/run'
 
 const exec = util.promisify(child.exec)
 
+function encrypt(buffer: Buffer, secret: string) {
+  const key = Buffer.alloc(32, secret, 'utf-8')
+  const iv = crypto.randomBytes(16)
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
+  const result = Buffer.concat([cipher.update(buffer), cipher.final()])
+  const output = `${iv.toString('hex')}:${result.toString('hex')}`
+
+  return output
+}
+
 describe('Terraform Apply', () => {
   const sha = crypto.createHash('sha1').digest('hex')
 
@@ -27,6 +37,8 @@ describe('Terraform Apply', () => {
   beforeEach(() => {
     delete process.env.GITHUB_WORKSPACE
     delete process.env.INPUT_PATH
+    delete process.env.INPUT_DECRYPT
+    delete process.env.SECRET
   })
 
   test('applies the working directory', async () => {
@@ -92,6 +104,40 @@ describe('Terraform Apply', () => {
       await exec('terraform init', { cwd })
       await exec('terraform plan -input=false -out=tfplan', { cwd })
       await fs.promises.stat(path.join(cwd, 'tfplan'))
+      await run()
+    } finally {
+      await cleanup()
+    }
+
+    stdout.stop()
+
+    expect(stdout.output).toMatch('Creation complete')
+  }, 20000)
+
+  test('applies an encrypted terraform plan', async () => {
+    const { path: cwd, cleanup } = await tmp.dir({ unsafeCleanup: true })
+
+    const src = path.join(__dirname, 'fixtures/terraform.tf')
+    const dst = path.join(cwd, 'terraform.tf')
+
+    const source = path.join(cwd, 'tfplan')
+    const target = path.join(cwd, 'tfplan.encrypted')
+
+    process.env.GITHUB_WORKSPACE = cwd
+    process.env.INPUT_PATH = target
+    process.env.INPUT_DECRYPT = 'true'
+    process.env.SECRET = 'secret'
+
+    stdout.start()
+
+    try {
+      await fs.promises.copyFile(src, dst)
+      await exec('terraform init', { cwd })
+      await exec('terraform plan -input=false -out=tfplan', { cwd })
+      const buffer = await fs.promises.readFile(source)
+      await fs.promises.writeFile(target, encrypt(buffer, 'secret'))
+      await fs.promises.unlink(source)
+      await fs.promises.unlink(dst)
       await run()
     } finally {
       await cleanup()
